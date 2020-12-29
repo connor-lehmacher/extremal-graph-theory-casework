856a9792a42e3df97fba396269760fb29e780a98
Connor Lehmacher <clehmac@hotmail.com>
2020-12-29 20:59:19 +0000

    used compute-ex to work on p4

--- compute-ex.py
+++ compute-ex.py
@@ -1,12 +1,15 @@
 from igraph import *
 from itertools import *
 
-i = Graph(directed = True)
-i.add_vertices(5)
-i.add_edges([(0,1),(1,2),(2,3),(3,4)]) #vec(P_5)
+i = Graph(directed = True) #ex(n, i)
+i.add_vertices(4)
+i.add_edges([(0,1),(1,2),(2,3)]) #vec(P_4)
 
 n = 7
-m = 30
+m = 17
+#n * (n-1)
+
+print("n = ",n)
 
 g = Graph(directed = True) #test graph
 g.add_vertices(n)
@@ -31,11 +34,10 @@ while not ifreegraphs:
         g.delete_edges(edgestobeadded)
     m = m - 1
 
+#remove duplicates up to isomorphism
 print("Removing duplicates")
-
 check = True
 
-#remove duplicates up to isomorphism
 for h in ifreegraphs:
     for k in ifreeisos:
         if h.isomorphic(k):
@@ -47,3 +49,4 @@ for h in ifreegraphs:
 #print graphs
 for h in ifreeisos:
     print(h)
+#   plot(h, bbox=(0,0,100,100))
