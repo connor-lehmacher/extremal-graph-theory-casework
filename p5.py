9000021d2e7a78e97c17fb6855e11de41cc16e20
Connor Lehmacher <clehmac@hotmail.com>
2021-01-05 00:53:38 +0000

    started building code to work on n = 8,9, 10

--- p5.py
+++ p5.py
@@ -1,70 +1,60 @@
 from igraph import *
 from itertools import *
 from random import random
+import math
 
-i = Graph(directed = True)
-i.add_vertices(5)
-i.add_edges([(0,1),(1,2),(2,3),(3,4)]) #vec(P_%)
-
-n = 8
-m = 24
-
-g = Graph(directed = True) #test graph
+#This is the graph we study
+n = 7
+m = 18
+checkmaximality = True
+g = Graph(directed = True) 
 g.add_vertices(n)
-Redges = []
-for x in permutations(range(4, n),2):
-    Redges.append(x)
-Pedges = []
-for x in permutations(range(0,4),2):
-    Pedges.append(x)
+#We can assume it contains a P_4
+g.add_edges([(0,1),(1,2),(2,3)])
+
+i = Graph(directed = True) #we compute ex(n,i)
+i.add_vertices(5)
+i.add_edges([(0,1),(1,2),(2,3),(3,4)]) #P_5
+j = Graph(directed = True)
+j.add_vertices(4)
+j.add_edges([(0,1),(1,2),(2,3),(3,0)]) #C_4
+#note that n should be >= 6 for the C_4-free assumption (for ex, for EX we need >= 7)
+
+#These are all the canidate edges
+edges = []
+for x in permutations(range(0, n),2):
+    edges.append(x)
 for a in range(0,3):
-    Pedges.remove((a, a+1))
-Pedges.remove((3,0)) #no cycles!
+    edges.remove((a, a+1))
+edges.remove((3,0)) #We can assume its C_4 free
+for a in range(4,n):
+    edges.remove((a,0))
+    edges.remove((3,a)) #either of these make a P_5
 
-print(Redges)
-print(Pedges)
+#loading bar code
+numberofcanidateedges = len(edges)
+numberofcombinations = math.factorial(numberofcanidateedges)/ (math.factorial(m-3) * math.factorial(numberofcanidateedges - m + 3))
+a = 0
+b = 0
+c = 1000
 
+#data structures necessary to algorithm
 edgestobeadded = []
 ifreegraphs = []
 
-#Add P4 to the graph
-g.add_edges([(0,1),(1,2),(2,3)])
-
-m1 = m - 3 #remove the P4 from the edges we  need to add
-
-#iterate through relavent graphs
-for m2 in range(0, min(m1, 6) + 1): #the choices for the number of extra edges in the P set
-    for m3 in range(0, min(m1 - m2, (n - 4) * 3) + 1): #the choices for the number of edges between P and R
-        m4 = m1 - m2 - m3 # the number of edges in the R set
-        print("Step:", m2, m3, m4)
-        for z in combinations(range(0, 3 * (n-4)),m3): #making the choices of which canidate spots in the P to R set are filled
-
-           for w in range(0, 2 ** m3): #think of w as a binary number that stores all the choices for which choice is made in each canidate spot
-                for x in combinations(Pedges, m2): #choosing P edges
-                    for y in combinations(Redges, m4): #choosing R edges
-                        for a in range(0, m3):
-                            b = list(z)[a] #gives spot information
-                            if w // 2**a % 2:
-                                g.add_edges([(b // 3 + 4, b % 3 + 1)])
-                            else:
-                                g.add_edges([(b % 3, b // 3 + 4)])
-                        if random() < 0.000005:
-                            print(g.get_edgelist())
-                        g.add_edges(list(x))
-                        g.add_edges(list(y))
-                        if not g.subisomorphic_vf2(i):
-                            ifreegraphs.append(g.copy())
-                        g.delete_edges(None)
-                        g.add_edges([(0,1),(1,2),(2,3)])
-
-
-#for x in combinations(edges, m):
-#    edgestobeadded = list(x)
-#    g.add_edges(edgestobeadded)
-#    # Checks if g has a copy of i using the vf2 algorithm
-#    if g.subisomorphic_vf2(i) == False:
-#        ifreegraphs.append(g.copy())
-#    g.delete_edges(None)
+#the checking loop
+for x in combinations(edges, m - 3):
+    edgestobeadded = list(x)
+    g.add_edges(edgestobeadded)
+    # Checks if g has a copy of i using the vf2 algorithm
+    if not g.subisomorphic_vf2(i):
+        ifreegraphs.append(g.copy())
+    g.delete_edges(None)
+    g.add_edges([(0,1),(1,2),(2,3)])
+    a = a + 1
+    if a/numberofcombinations * c >= b:
+        print(b/c)
+        b = b + 1
 
 
 #remove duplicates up to isomorphism and duality
@@ -89,4 +79,21 @@ for h in ifreegraphs:
 for h in ifreeisos:
     print(h)
 if not ifreeisos:
+    checkmaximality = false
     print("Above extremal number")
+
+#check ex using maximality, the proposition is in my notes
+maximalityedges = []
+check = True
+if checkmaximality:
+    for k in ifreeisos:
+       maximalityedges = list(set(edges) - set(k.get_edgelist()))
+       for e in maximalityedges:
+           k.add_edges([e])
+           if not k.subisomorphic_vf2(i):
+               check = False
+           k.delete_edges([e])
+    if check:
+        print("At extremal number")
+    else:
+        print("Below extremal number")
